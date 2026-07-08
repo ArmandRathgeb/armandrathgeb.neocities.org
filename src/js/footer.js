2f2a7687a8d12e3713b126a64d12364bd22900eb
ArmandRathgeb <armand.rathgeb@utsouthwestern.edu>
2026-07-08 12:59:14 -0500

    Some fixes, maybe mobile fix

--- src/js/footer.js
+++ src/js/footer.js
@@ -7,6 +7,11 @@ class Footer extends HTMLElement {
      this.innerHTML = `
     <img src="/resources/kurisu_sunglasses_transparent.png" class="footer-img-l">
     <footer class="main-content">
+      Armand Rathgeb, 2026- 
+      <div class="vr"></div>
+      <a href="/about.html">Contact me</a> 
+      <div class="vr"></div>
+      <a href="/pgp.html">PGP</a>
     <footer>
     `;
   }
